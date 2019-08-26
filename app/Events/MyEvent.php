82e5d6ef066f550c190138e27220f3bc896797fd
Noud de Brouwer <noud4@home.nl>
2019-08-26 22:14:01 +0000

    Initial commit.

---
+++ app/Events/MyEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MyEvent implements ShouldBroadcast
+{
+  use Dispatchable, InteractsWithSockets, SerializesModels;
+
+  public $message;
+
+  public function __construct($message)
+  {
+      $this->message = $message;
+  }
+
+  public function broadcastOn()
+  {
+      return ['my-channel'];
+  }
+
+  public function broadcastAs()
+  {
+      return 'my-event';
+  }
+}
